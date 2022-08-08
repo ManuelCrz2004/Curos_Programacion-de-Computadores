d0fe0ef848d4d6b06a682439e3ca075b221a9d0a
Prestamo Estudiantes <prestamour@urosario.edu.co>
2022-08-08 08:27:29 -0500

    Se termina la clase del 8 de Agosto

---
+++ clase_1/calcular_area_circulo.py
@@ -0,0 +1,9 @@
+import math   # libreria la cual permite usar temrminos matematicos como PI
+
+radio = 100
+area = (math.pi * (radio**2))
+perimetro = (2 * math.pi * radio)
+pi = math.pi
+
+print("area:", area)
+print("perimetro", perimetro)
---
+++ clase_1/manipular_variables.py
@@ -0,0 +1,8 @@
+
+
+a = 4
+b = 3
+c = (a**2 + b**2)
+d = (c**0.5)
+
+print("Este es el valor de D:", d)
