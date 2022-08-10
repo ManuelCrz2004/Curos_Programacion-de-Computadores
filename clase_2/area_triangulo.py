178777eb96d04a7695c8dc2226f6233997211eb1
Prestamo Estudiantes <prestamour@urosario.edu.co>
2022-08-10 08:00:43 -0500

    Se agragan los ejercicios de area de triangulo e interaccion con el ususario, estos se realizaron en la primera hora de la clase 2

---
+++ clase_2/area_triangulo.py
@@ -0,0 +1,9 @@
+base = input("Ingrese el valor de la base: ")
+altura = input("Ingrese el valor de al altura")
+
+base = int(base)
+altura = int(altura)
+
+calcular_area = (base*altura) / 2
+
+print("El area del triangulo es: ", calcular_area)
---
+++ clase_2/interaccion_usuario.py
@@ -0,0 +1,3 @@
+nombre_usuario = input("ingrese el nombre de usuario")
+
+print(nombre_usuario)
