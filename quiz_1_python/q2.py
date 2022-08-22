0d9f05332834710105dd8df4c2222df5bb0e8cfc
Prestamo Estudiantes <prestamour@urosario.edu.co>
2022-08-22 06:31:25 -0500

    Se anexa la carpeta en la cual se encuentra el quiz #1 de programación

---
+++ quiz_1_python/q2.py
@@ -0,0 +1,15 @@
+# Punto número 1. Quiz #2 Python
+
+# Estudiante Manuel Santiago Cruz Garrote, Grp1 Programación de computadores
+
+segundos_usuario = int(input("Ingrese el número de segundos a convertir: "))
+
+calculo_minutos_int = (segundos_usuario // 60)
+calculo_minutos = str(calculo_minutos_int)
+calculo_segundos_int = (segundos_usuario % 60)
+calculo_segundos = str(calculo_segundos_int)
+
+if 
+
+print("Hay un total de " + calculo_minutos + " minuto(s) y " + calculo_segundos + " segundos")
+    
